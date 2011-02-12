136ac133a72607ddefb4833724dd84189564b631
Andrea Franz <andrea@gravityblast.com>
2011-02-12 18:01:23 -0500

    .

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,9 +2,20 @@ class TracksController < ApplicationController
   before_filter :find_playlist
   
   def create
-    @track = @playlist.tracks.build(params[:track])
-    if @track.save
-      flash[:notice] = 'Track added successfully'      
+    mxm_track_result  = MusixMatch.get_track(params[:mxm_id])
+    mxm_lyrics_result = MusixMatch.get_lyrics(params[:mxm_id])
+    mxm_track = mxm_track_result.track
+    mxm_lyrics = mxm_lyrics_result.lyrics
+    if mxm_track && mxm_lyrics
+      @track = @playlist.tracks.build
+      @track.mxm_id = mxm_track.track_id
+      @track.name = mxm_track.track_name
+      @track.artist = mxm_track.artist_name
+      if @track.save
+        flash[:notice] = 'Track added successfully'
+      else
+        flash[:error] = 'Problem adding track'
+      end
     else
       flash[:error] = 'Problem adding track'
     end
@@ -19,7 +30,7 @@ class TracksController < ApplicationController
   end
   
   def search
-    @tracks_result = MusixMatch.search_track(:q => params[:q])
+    @tracks_result = MusixMatch.search_track(:q => params[:q], :f_has_lyrics => 1)
     render :layout => false
     # render :json => @tracks_result.track_list.to_json
   end
