ccd64eb9af6f0eda0af94c601c3bfa84fe2e2613
Andrea Franz <andrea@gravityblast.com>
2011-02-13 13:34:52 -0500

    .

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,7 +1,7 @@
 module TracksHelper
   def formatted_lyrics_body(mxm_lyrics)
     html = ""
-    mxm_lyrics.lyrics_body.split(/\n/).each_with_index do |line, index|
+    mxm_lyrics.lyrics_body.split(/[\n\r]+/).each_with_index do |line, index|
       html << %|<p class="line line-#{index + 1}">#{line}</p>|
     end
     html.html_safe
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,7 +2,7 @@ class Playlist < ActiveRecord::Base
   attr_accessible :name
   
   belongs_to :user
-  has_many :tracks  
+  has_many :tracks, :dependent => :destroy  
   
   validates :user_id, :presence => true 
   validates :name, :presence => true 
